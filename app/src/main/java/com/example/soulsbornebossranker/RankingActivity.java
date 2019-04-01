bd70a3a623c2f453878c5c0231b2b87b2a8864ec
psyjb12 <psyjb12@nottingham.ac.uk>
2019-04-01 16:59:33 +0100

    Included ranking number in table. Passable scaling on ranking page.

--- app/src/main/java/com/example/soulsbornebossranker/RankingActivity.java
+++ app/src/main/java/com/example/soulsbornebossranker/RankingActivity.java
@@ -3,6 +3,7 @@ package com.example.soulsbornebossranker;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +11,6 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -50,8 +50,10 @@ public class RankingActivity extends AppCompatActivity {
         navigation.getMenu().getItem(2).setChecked(true);
 
         ranking_layout = (LinearLayout) findViewById(R.id.ranking_layout);
-        ranking_layout.addView(createRowCard("ds1", R.string.boss_name, R.string.elo_value));
-        ranking_layout.addView(createRowCard("ds3", R.string.boss_name2, R.string.elo_value));
+        for(int i = 0; i < 10; i++) {
+            ranking_layout.addView(createRow("1", "ds1", R.string.boss_name, R.string.elo_value));
+            ranking_layout.addView(createRow("2", "ds3", R.string.boss_name2, R.string.elo_value));
+        }
 
     }
 
@@ -65,11 +67,28 @@ public class RankingActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public CardView createRowCard(String game, int name_string_id, int elo_string_id) {
+    public LinearLayout createRow(String rank, String game, int name_string_id, int elo_string_id) {
         Context context = getApplicationContext();
 
+        LinearLayout outer_ll = new LinearLayout(context);
+        LinearLayout.LayoutParams outerLLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        outerLLParams.setMargins(0, 0,0,10);
+        outer_ll.setLayoutParams(outerLLParams);
+        outer_ll.setBackgroundColor(Color.BLACK);
+
+        TextView rank_tv = new TextView(context);
+        LinearLayout.LayoutParams rankParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 0.05f);
+        rank_tv.setLayoutParams(rankParams);
+        rank_tv.setText(rank);
+        rank_tv.setTextColor(Color.WHITE);
+        rank_tv.setTextSize(18);
+        rank_tv.setTextAppearance(Typeface.BOLD);
+        rank_tv.setPadding(8,0,0,0);
+        rank_tv.setGravity(Gravity.CENTER);
+        rank_tv.setGravity(Gravity.CENTER_VERTICAL);
+
         CardView cv = new CardView(context);
-        LinearLayout.LayoutParams cvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams cvParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.95f);
         cv.setLayoutParams(cvParams);
         cv.setCardBackgroundColor(Color.BLACK);
 
@@ -80,7 +99,7 @@ public class RankingActivity extends AppCompatActivity {
         ll.setOrientation(LinearLayout.HORIZONTAL);
 
         ImageView iv = new ImageView(context);
-        LinearLayout.LayoutParams ivParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.12f);
+        LinearLayout.LayoutParams ivParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.11f);
         iv.setLayoutParams(ivParams);
         iv.setAdjustViewBounds(true);
         if(game.contains("ds1"))
@@ -93,10 +112,12 @@ public class RankingActivity extends AppCompatActivity {
             iv.setImageResource(R.drawable.bb);
 
         TextView name_tv = new TextView(context);
-        LinearLayout.LayoutParams nameParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.85f);
+        LinearLayout.LayoutParams nameParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 0.85f);
         name_tv.setLayoutParams(nameParams);
         name_tv.setText(name_string_id);
         name_tv.setTextColor(Color.WHITE);
+        name_tv.setTextSize(12);
+        name_tv.setMaxLines(2);
 
         TextView elo_tv = new TextView(context);
         LinearLayout.LayoutParams eloParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.15f);
@@ -104,12 +125,17 @@ public class RankingActivity extends AppCompatActivity {
         elo_tv.setText(elo_string_id);
         elo_tv.setTextColor(Color.WHITE);
         elo_tv.setTextSize(18);
+        elo_tv.setGravity(Gravity.CENTER);
+        elo_tv.setGravity(Gravity.CENTER_VERTICAL);
 
         ll.addView(iv);
         ll.addView(name_tv);
         ll.addView(elo_tv);
         cv.addView(ll);
 
-        return cv;
+        outer_ll.addView(rank_tv);
+        outer_ll.addView(cv);
+
+        return outer_ll;
     }
 }
