e86c6d367949b195f3f77cc7a6fe69ccd2d780dc
JPUF <2009bennettj@gmail.com>
2019-04-10 11:30:40 +0100

    A failed attempt to implement Elo.

--- app/src/main/java/com/example/soulsbornebossranker/Contest.java
+++ app/src/main/java/com/example/soulsbornebossranker/Contest.java
@@ -1,6 +1,7 @@
 package com.example.soulsbornebossranker;
 
 import android.util.Log;
+import java.lang.Math;
 
 public class Contest {
 
@@ -8,5 +9,17 @@ public class Contest {
 
     public static void scoreResult(Boss winner, Boss loser) {
         Log.i("Result", "winner: " + winner.name + " loser: " + loser.name);
+        int winnerPoints = winner.points;
+        int loserPoints = loser.points;
+        winnerPoints = (int) Math.pow(10,winnerPoints/400);
+        loserPoints = (int) Math.pow(10,loserPoints/400);
+        int winnerExpected = winnerPoints / (winnerPoints + loserPoints);
+        int loserExpected = loserPoints / (loserPoints + winnerPoints);
+
+        winner.points = winnerPoints + 32 * (1 - winnerExpected);
+        loser.points = loserPoints + 32 * (0 - loserExpected);
+
+        Log.i("score", "winner: "+winner.points + " loser: "+loser.points);
+
     }
 }
