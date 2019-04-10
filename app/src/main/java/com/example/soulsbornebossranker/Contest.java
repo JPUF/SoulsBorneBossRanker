0e1186a2f6478175c6fcf981b537b1ed50c546ff
JPUF <2009bennettj@gmail.com>
2019-04-10 12:06:11 +0100

    Better implementation of Elo, still not writing to DB.

--- app/src/main/java/com/example/soulsbornebossranker/Contest.java
+++ app/src/main/java/com/example/soulsbornebossranker/Contest.java
@@ -9,15 +9,13 @@ public class Contest {
 
     public static void scoreResult(Boss winner, Boss loser) {
         Log.i("Result", "winner: " + winner.name + " loser: " + loser.name);
-        int winnerPoints = winner.points;
-        int loserPoints = loser.points;
-        winnerPoints = (int) Math.pow(10,winnerPoints/400);
-        loserPoints = (int) Math.pow(10,loserPoints/400);
-        int winnerExpected = winnerPoints / (winnerPoints + loserPoints);
-        int loserExpected = loserPoints / (loserPoints + winnerPoints);
 
-        winner.points = winnerPoints + 32 * (1 - winnerExpected);
-        loser.points = loserPoints + 32 * (0 - loserExpected);
+        double qWinner = Math.pow(10,((double) winner.points / 400.0d));
+        double qLoser = Math.pow(10,((double) loser.points / 400.0d));
+        double expectedWinner = qWinner / (qWinner + qLoser);
+        double expectedLoser = qLoser / (qLoser + qWinner);
+        winner.points = winner.points + 32 * (1 - (int) expectedWinner);
+        loser.points = loser.points + 32 * (0 - (int) expectedLoser);
 
         Log.i("score", "winner: "+winner.points + " loser: "+loser.points);
 
